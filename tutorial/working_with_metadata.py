eeca6fc6fa1739883be6238032fe5aa20bab9b6b
Ruslan Hamidullin <ruslan.hamidullin@starnavi.io>
2021-08-16 17:46:44 +0300

    Working with metadata and creating tables with ORM and core

---
+++ tutorial/working_with_metadata.py
@@ -0,0 +1,47 @@
+from sqlalchemy import MetaData, ForeignKey, create_engine
+from sqlalchemy import Table, Column, Integer, String
+from sqlalchemy.orm import declarative_base, relationship
+
+engine = create_engine("sqlite+pysqlite:///:memory:", echo=True, future=True)
+metadata = MetaData()
+
+# define using Table and Column
+user_table = Table(
+    "user_account",
+    metadata,
+    Column('id', Integer, primary_key=True),
+    Column('name', String(30)),
+    Column('fullname', String)
+)
+address_table = Table(
+    "address",
+    metadata,
+    Column('id', Integer, primary_key=True),
+    Column('user_id', ForeignKey('user_account.id'), nullable=False),
+    Column('email_address', String, nullable=False)
+)
+# metadata.create_all(engine)
+
+Base = declarative_base()
+
+class User(Base):
+    __tablename__ = 'user_account'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(30))
+    fullname = Column(String)
+    addresses = relationship("Address", back_populates="user")
+
+    def __repr__(self):
+        return f"User(id={self.id!r}, name={self.name!r}, fullname={self.fullname!r})"
+
+
+class Address(Base):
+    __tablename__ = 'address'
+    id = Column(Integer, primary_key=True)
+    email_address = Column(String, nullable=False)
+    user_id = Column(Integer, ForeignKey('user_account.id'))
+    user = relationship("User", back_populates="addresses")
+
+    def __repr__(self):
+        return f"Address(id={self.id!r}, email_address={self.email_address!r})"
+Base.metadata.create_all(engine)
